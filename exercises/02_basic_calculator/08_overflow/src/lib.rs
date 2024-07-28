b96681a912fc4ffe324eecb7831d08f400b6c0af
saguillo2000 <oriolsaguilo@gmail.com>
2024-07-28 11:31:03 +0200

    calculator 02 and intro 01

--- exercises/02_basic_calculator/08_overflow/src/lib.rs
+++ exercises/02_basic_calculator/08_overflow/src/lib.rs
@@ -4,7 +4,7 @@
 //
 // For reasons that we'll explain later, the customization needs to be done in the `Cargo.toml`
 // at the root of the repository, not in the `Cargo.toml` of the exercise.
-
+#[allow(overflowing_literals)]
 pub fn factorial(n: u32) -> u32 {
     let mut result = 1;
     for i in 1..=n {
