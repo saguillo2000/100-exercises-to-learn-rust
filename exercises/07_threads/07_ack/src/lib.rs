610e38b36fe5c91bfac1a03e146f96e85d38658d
saguillo2000 <oriolsaguilo@gmail.com>
2024-09-29 16:20:06 +0200

    final exercises

--- exercises/07_threads/07_ack/src/lib.rs
+++ exercises/07_threads/07_ack/src/lib.rs
@@ -6,8 +6,8 @@ pub mod store;
 
 // Refer to the tests to understand the expected schema.
 pub enum Command {
-    Insert { todo!() },
-    Get { todo!() }
+    Insert { draft: data::TicketDraft, response_sender: Sender<store::TicketId> },
+    Get { id: store::TicketId, response_sender: Sender<Option<data::Ticket>> },
 }
 
 pub fn launch() -> Sender<Command> {
@@ -21,13 +21,19 @@ pub fn server(receiver: Receiver<Command>) {
     let mut store = TicketStore::new();
     loop {
         match receiver.recv() {
-            Ok(Command::Insert {}) => {
-                todo!()
+            Ok(Command::Insert {
+                draft,
+                response_sender,
+            }) => {
+                let id = store.add_ticket(draft);
+                response_sender.send(id).unwrap();
             }
             Ok(Command::Get {
-                todo!()
+                id,
+                response_sender,
             }) => {
-                todo!()
+                let ticket = store.get(id);
+                response_sender.send(ticket.cloned()).unwrap();
             }
             Err(_) => {
                 // There are no more senders, so we can safely break
